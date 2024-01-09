8f0f3b7cc14c08572bfc3af8672bee4831d56d8a
omphileT-creator <155839199+omphileT-creator@users.noreply.github.com>
2024-01-09 21:09:23 +0200

    added a function to get a forecast

--- index.js
+++ index.js
@@ -16,6 +16,7 @@
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
     temperatureElement.innerHTML = Math.round(temperature);
     iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+    getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -51,7 +52,12 @@ function handleSearchSubmit(event) {
 
     searchCity(searchInput.value);
 }
-function displayForecast() {
+function getForecast(city) {
+    let apiKey = "4a7075563b0eebo45t378ca235ffa601";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios(apiUrl).then(displayForecast);
+}
+function displayForecast(response) {
     let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
     let forecastHtml = "";
 
@@ -81,4 +87,5 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Sandton");
-displayForecast();
+
+
